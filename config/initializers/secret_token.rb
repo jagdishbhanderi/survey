1195538e00632500ab7494224e8d4efe34e3413a
Krunal Babaria <krunal@greyvoid.com>
2013-11-21 15:53:40 +0530

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SocialDemoApp::Application.config.secret_key_base = '4786baeef0f826de5b136a6e2e558c5232983104159a443ebce4a4f017e8a9872050153fa75311b48d2a6d102ca37b2cc9cd9655314f34ef3b8ae8c3f19b8fc0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SocialDemoApp::Application.config.session_store :cookie_store, key: '_social_demo_app_session'
